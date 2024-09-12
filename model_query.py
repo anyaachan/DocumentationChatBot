8231891b33c31993725e346b9fe549842ce39f29
anyaachan <anna.danchenko@icloud.com>
2024-09-12 04:12:34 +0200

    add chat history

--- model_query.py
+++ model_query.py
@@ -9,20 +9,23 @@ from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains import create_history_aware_retriever
 from langchain_core.prompts import MessagesPlaceholder
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.messages import HumanMessage
 load_dotenv()
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-RAG_TEMPLATE = """
-You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+chat_history = []
+question = "What is image to image API?"
+follow_up_question = "What are the schedulers avaliable for it?"
 
-<context>
-{context}
-</context>
+rag_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
 
-Answer the following question:
-
-{question}"""
+{context}"""
 
 contextualize_system_prompt =  (
     "Given a chat history and the latest user question "
@@ -32,10 +35,15 @@ contextualize_system_prompt =  (
     "just reformulate it if needed and otherwise return it as is."
 )
 
-question = "Name all of the schedulers in text to video stable diffusion endpoint"
 model = ChatOpenAI()
 
-rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+rag_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", rag_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("user", "{input}"),
+    ]
+)
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 vector_store = Chroma(collection_name="StableDiffusionAPIDocs",
@@ -49,9 +57,10 @@ contextualize_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", contextualize_system_prompt),
         MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
+        ("user", "{input}"), # question
     ]
 )
+
 history_aware_retriever = create_history_aware_retriever(
     model, retriever, contextualize_prompt
 )
@@ -59,11 +68,12 @@ history_aware_retriever = create_history_aware_retriever(
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | rag_prompt
-    | model
-    | StrOutputParser()
-)
+question_answer_chain = create_stuff_documents_chain(model, rag_prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-print(chain.invoke(question))
+
+answer_01 = rag_chain.invoke({"input": question, "chat_history": chat_history})
+chat_history.extend([HumanMessage(content=question), answer_01["answer"]]) # ?
+answer_02 = rag_chain.invoke({"input": follow_up_question, "chat_history": chat_history})
+print(answer_01["answer"])
+print(answer_02["answer"])
